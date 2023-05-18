74642cc7851c51a0e679fc392128492f24fb2dcb
Rishab Garg <rishgarg@gmail.com>
2023-05-18 12:11:30 +0200

    test: update tests

--- test/elixir_ml_test.exs
+++ test/elixir_ml_test.exs
@@ -2,7 +2,8 @@ defmodule ElixirMLTest do
   use ExUnit.Case
   doctest ElixirML
 
-  test "greets the world" do
-    assert ElixirML.hello() == :world
+  # TODO
+  test "trains the model" do
+    true
   end
 end
